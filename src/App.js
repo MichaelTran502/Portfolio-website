d1c8bd14729419306c49526d325c43fd9d1da51b
Michael Tran <michael.tran2208@gmail.com>
2021-01-25 14:58:20 +1100

    created navbar with bootstrap, dark themed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// components
+import MyNavbar from './components/mynavbar';
+
 const App = () => {
   return (
     <div>
+      <MyNavbar />
       My Portfolio
     </div>
   );
