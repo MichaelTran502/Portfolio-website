1ee05abf3d213c828525a5b783875cd98dee1f03
Michael Tran <michael.tran2208@gmail.com>
2021-01-27 17:42:29 +1100

    Added carousel page using react bootstrap

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 
 // components
-import MyNavbar from './components/mynavbar';
+import MyNavbar from './components/my-navbar/mynavbar';
+import MyCarousel from './components/my-carousel/my-carousel';
 
 const App = () => {
   return (
     <div>
       <MyNavbar />
-      My Portfolio
+      <MyCarousel />
     </div>
   );
 }
