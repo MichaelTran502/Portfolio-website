c2a831bbcedd8470bc932d3e3c090fb240d06e76
Michael Tran <michael.tran2208@gmail.com>
2021-01-27 17:52:45 +1100

    implemented intro message component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import './App.css';
 // components
 import MyNavbar from './components/my-navbar/mynavbar';
 import MyCarousel from './components/my-carousel/my-carousel';
+import Intro from './components/my-intro/intro';
 
 const App = () => {
   return (
     <div>
       <MyNavbar />
       <MyCarousel />
+      <Intro />
     </div>
   );
 }
